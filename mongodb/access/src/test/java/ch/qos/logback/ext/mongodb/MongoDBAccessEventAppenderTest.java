a3d0ffd481d8a56eba72a88e10bf15ca15b6649b
Christian Trutz <christian.trutz@belaso.de>
2012-06-22 23:50:03 +0200

    More unit tests for MongoDBAccessEventAppender.
    Signed-off-by: Christian Trutz <christian.trutz@belaso.de>

--- mongodb/access/src/test/java/ch/qos/logback/ext/mongodb/MongoDBAccessEventAppenderTest.java
+++ mongodb/access/src/test/java/ch/qos/logback/ext/mongodb/MongoDBAccessEventAppenderTest.java
@@ -74,6 +74,62 @@ public class MongoDBAccessEventAppenderTest {
         assertEquals("servername", dbObject.get("serverName"));
     }
 
+    @Test
+    public void testRemote() {
+        // given
+        new NonStrictExpectations() {
+            {
+                event.getRemoteHost();
+                result = "host";
+                event.getRemoteUser();
+                result = "user";
+                event.getRemoteAddr();
+                result = "addr";
+            }
+        };
+        // when
+        final BasicDBObject dbObject = appender.toMongoDocument(event);
+        // then
+        BasicDBObject remoteDBObject = (BasicDBObject) dbObject.get("remote");
+        assertEquals("host", remoteDBObject.getString("host"));
+        assertEquals("user", remoteDBObject.getString("user"));
+        assertEquals("addr", remoteDBObject.getString("addr"));
+    }
+
+    @Test
+    public void testRequest() {
+        // given
+        new NonStrictExpectations() {
+            {
+                event.getRequestURI();
+                result = "uri";
+                event.getProtocol();
+                result = "protocol";
+                event.getMethod();
+                result = "method";
+                event.getRequestContent();
+                result = "postContent";
+                event.getCookie("JSESSIONID");
+                result = "sessionId";
+                event.getRequestHeader("User-Agent");
+                result = "userAgent";
+                event.getRequestHeader("Referer");
+                result = "referer";
+            }
+        };
+        // when
+        final BasicDBObject dbObject = appender.toMongoDocument(event);
+        // then
+        BasicDBObject requestDBObject = (BasicDBObject) dbObject.get("request");
+        assertEquals("uri", requestDBObject.getString("uri"));
+        assertEquals("protocol", requestDBObject.getString("protocol"));
+        assertEquals("method", requestDBObject.getString("method"));
+        assertEquals("postContent", requestDBObject.getString("postContent"));
+        assertEquals("sessionId", requestDBObject.getString("sessionId"));
+        assertEquals("userAgent", requestDBObject.getString("userAgent"));
+        assertEquals("referer", requestDBObject.getString("referer"));
+    }
+
     //
     //
     // MOCKING
