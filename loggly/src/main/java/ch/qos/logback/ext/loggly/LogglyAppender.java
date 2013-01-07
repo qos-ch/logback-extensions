e3bb5cb9a01d4990e2f80d4b6d976ae809764ca7
Cyrille Le Clerc <cyrille@cyrilleleclerc.com>
2013-01-08 00:31:54 +0100

    Add LogglyBatchAppender

--- loggly/src/main/java/ch/qos/logback/ext/loggly/LogglyAppender.java
+++ loggly/src/main/java/ch/qos/logback/ext/loggly/LogglyAppender.java
@@ -15,14 +15,7 @@
  */
 package ch.qos.logback.ext.loggly;
 
-import ch.qos.logback.classic.PatternLayout;
-import ch.qos.logback.core.Context;
-import ch.qos.logback.core.Layout;
-import ch.qos.logback.core.UnsynchronizedAppenderBase;
-
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -34,18 +27,7 @@ import java.net.URL;
  * @author Les Hazlewood
  * @since 0.1
  */
-public class LogglyAppender<E> extends UnsynchronizedAppenderBase<E> {
-
-    public static final String DEFAULT_ENDPOINT_PREFIX = "https://logs.loggly.com/inputs/";
-
-    public static final String DEFAULT_LAYOUT_PATTERN = "%d{\"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'\",UTC} %-5level [%thread] %logger: %m%n";
-
-    private String endpointUrl;
-    private String inputKey;
-
-    private Layout<E> layout;
-    private boolean layoutCreatedImplicitly = false;
-    private String pattern;
+public class LogglyAppender<E> extends AbstractLogglyAppender<E> {
 
     public LogglyAppender() {
     }
@@ -77,129 +59,12 @@ public class LogglyAppender<E> extends UnsynchronizedAppenderBase<E> {
         }
     }
 
-    private String readResponseBody(final InputStream input) throws IOException {
-        try {
-            final byte[] bytes = toBytes(input);
-            return new String(bytes, "UTF-8");
-        } finally {
-            input.close();
-        }
-    }
-
-    private byte[] toBytes(final InputStream is) throws IOException {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        int count;
-        byte[] buf = new byte[512];
-
-        while((count = is.read(buf, 0, buf.length)) != -1) {
-            baos.write(buf, 0, count);
-        }
-        baos.flush();
-
-        return baos.toByteArray();
-    }
-
     private void sendAndClose(final String event, final OutputStream output) throws IOException {
         try {
             output.write(event.getBytes("UTF-8"));
         } finally {
             output.close();
         }
-    }
-
-    protected final void ensureLayout() {
-        if (this.layout == null) {
-            this.layout = createLayout();
-            this.layoutCreatedImplicitly = true;
-        }
-        if (this.layout != null) {
-            Context context = this.layout.getContext();
-            if (context == null) {
-                this.layout.setContext(getContext());
-            }
-        }
-    }
-
-    protected Layout<E> createLayout() {
-        PatternLayout layout = new PatternLayout();
-        String pattern = getPattern();
-        if (pattern == null) {
-            pattern = DEFAULT_LAYOUT_PATTERN;
-        }
-        layout.setPattern(pattern);
-        return (Layout<E>) layout;
-    }
-
-    protected String buildEndpointUrl(String inputKey) {
-        return new StringBuilder(DEFAULT_ENDPOINT_PREFIX).append(inputKey).toString();
-    }
-
-    @Override
-    public void start() {
-        ensureLayout();
-        if (!this.layout.isStarted()) {
-            this.layout.start();
-        }
-        if (this.endpointUrl == null) {
-            if (this.inputKey == null) {
-                addError("inputKey (or alternatively, endpointUrl) must be configured");
-            } else {
-                this.endpointUrl = buildEndpointUrl(this.inputKey);
-            }
-        }
-        super.start();
-    }
-
-    @Override
-    public void stop() {
-        super.stop();
-        if (this.layoutCreatedImplicitly) {
-            try {
-                this.layout.stop();
-            } finally {
-                this.layout = null;
-                this.layoutCreatedImplicitly = false;
-            }
-        }
-    }
-
-    public String getEndpointUrl() {
-        return endpointUrl;
-    }
-
-    public void setEndpointUrl(String endpointUrl) {
-        this.endpointUrl = endpointUrl;
-    }
-
-    public String getInputKey() {
-        return inputKey;
-    }
-
-    public void setInputKey(String inputKey) {
-        String cleaned = inputKey;
-        if (cleaned != null) {
-            cleaned = cleaned.trim();
-        }
-        if ("".equals(cleaned)) {
-            cleaned = null;
-        }
-        this.inputKey = cleaned;
-    }
-
-    public String getPattern() {
-        return pattern;
-    }
-
-    public void setPattern(String pattern) {
-        this.pattern = pattern;
-    }
-
-    public Layout<E> getLayout() {
-        return layout;
-    }
-
-    public void setLayout(Layout<E> layout) {
-        this.layout = layout;
     }
 }
 
---
+++ loggly/src/main/java/ch/qos/logback/ext/loggly/io/DiscardingRollingOutputStream.java
@@ -0,0 +1,247 @@
+/*
+ * Copyright 2008-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ch.qos.logback.ext.loggly.io;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * <p>
+ * Capped in-memory {@linkplain OutputStream} composed of a chain of {@linkplain ByteArrayOutputStream} called 'buckets'.
+ * </p>
+ * <p>
+ * Each 'bucket' is limited in size (see {@link #maxBucketSizeInBytes}) and the total size of the {@linkplain OutputStream}
+ * is bounded thanks to a discarding policy. And external component is expected to consume the filled buckets thanks to
+ * {@link #getFilledBuckets()}.
+ * </p>
+ * <p>
+ * Implementation decisions:
+ * </p>
+ * <ul>
+ * <li>Why in-memory without offload on disk: offload on disk was possible with Google Guava's
+ * <code>FileBackedOutputStream</code> but had the drawback to introduce a dependency. Loggly bacth appender usecase
+ * should be OK with a pure in-memory approach.</li>
+ * </ul>
+ *
+ * @author <a href="mailto:cleclerc@xebia.fr">Cyrille Le Clerc</a>
+ */
+public class DiscardingRollingOutputStream extends OutputStream {
+
+    public static final String LINE_SEPARATOR = System.getProperty("line.separator");
+
+    private ByteArrayOutputStream currentBucket;
+
+    private final ReentrantLock currentBucketLock = new ReentrantLock();
+
+    private final BlockingDeque<ByteArrayOutputStream> filledBuckets;
+
+    private final ConcurrentLinkedQueue<ByteArrayOutputStream> recycledBucketPool;
+
+    private long maxBucketSizeInBytes;
+
+    private final AtomicInteger discardedBucketCount = new AtomicInteger();
+
+    /**
+     * @param maxBucketSizeInBytes
+     * @param maxBucketCount
+     */
+    public DiscardingRollingOutputStream(int maxBucketSizeInBytes, int maxBucketCount) {
+        if (maxBucketCount < 2) {
+            throw new IllegalArgumentException("'maxBucketCount' must be >1");
+        }
+
+        this.maxBucketSizeInBytes = maxBucketSizeInBytes;
+        this.filledBuckets = new LinkedBlockingDeque<ByteArrayOutputStream>(maxBucketCount);
+
+        this.recycledBucketPool = new ConcurrentLinkedQueue<ByteArrayOutputStream>();
+        this.currentBucket = newBucket();
+    }
+
+
+    @Override
+    public void write(int b) throws IOException {
+        currentBucketLock.lock();
+        try {
+            currentBucket.write(b);
+            rollCurrentBucketIfNeeded();
+        } finally {
+            currentBucketLock.unlock();
+        }
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        currentBucketLock.lock();
+        try {
+            currentBucket.write(b);
+            rollCurrentBucketIfNeeded();
+        } finally {
+            currentBucketLock.unlock();
+        }
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        currentBucketLock.lock();
+        try {
+            currentBucket.write(b, off, len);
+            rollCurrentBucketIfNeeded();
+        } finally {
+            currentBucketLock.unlock();
+        }
+    }
+
+    @Override
+    public void flush() throws IOException {
+        currentBucketLock.lock();
+        try {
+            currentBucket.flush();
+        } finally {
+            currentBucketLock.unlock();
+        }
+    }
+
+    /**
+     * Close all the underlying buckets (current bucket, filled buckets and buckets from the recycled buckets pool).
+     */
+    @Override
+    public void close() {
+        // no-op as ByteArrayOutputStream#close() is no op
+    }
+
+    /**
+     * Roll current bucket if size threshold has been reached.
+     */
+    private void rollCurrentBucketIfNeeded() {
+        if (currentBucket.size() < maxBucketSizeInBytes) {
+            return;
+        }
+        rollCurrentBucket();
+    }
+
+    /**
+     * Roll current bucket if size threshold has been reached.
+     */
+    public void rollCurrentBucketIfNotEmpty() {
+        if (currentBucket.size() == 0) {
+            return;
+        }
+        rollCurrentBucket();
+    }
+
+    /**
+     * Moves the current active bucket to the list of filled bucket and defines a new one.
+     * <p/>
+     * The new active bucket is reused from the {@link #recycledBucketPool} pool if one is available or recreated.
+     */
+    public void rollCurrentBucket() {
+        currentBucketLock.lock();
+        try {
+            boolean offered = filledBuckets.offer(currentBucket);
+            if (offered) {
+                onBucketRoll(currentBucket);
+            } else {
+                onBucketDiscard(currentBucket);
+                discardedBucketCount.incrementAndGet();
+            }
+
+            currentBucket = newBucket();
+        } finally {
+            currentBucketLock.unlock();
+        }
+    }
+
+    /**
+     * Designed for extension.
+     *
+     * @param discardedBucket the discarded bucket
+     */
+    protected void onBucketDiscard(ByteArrayOutputStream discardedBucket) {
+
+    }
+
+    /**
+     * The rolled bucket. Designed for extension.
+     *
+     * @param rolledBucket the discarded bucket
+     */
+    protected void onBucketRoll(ByteArrayOutputStream rolledBucket) {
+
+    }
+
+    /**
+     * Get a new bucket from the {@link #recycledBucketPool} or instantiate a new one if none available
+     * in the free bucket pool.
+     *
+     * @return the bucket ready to use
+     */
+    protected ByteArrayOutputStream newBucket() {
+        ByteArrayOutputStream bucket = recycledBucketPool.poll();
+        if (bucket == null) {
+            bucket = new ByteArrayOutputStream();
+        }
+        return bucket;
+    }
+
+    /**
+     * Returns the given bucket to the pool of free buckets.
+     *
+     * @param bucket the bucket to recycle
+     */
+    public void recycleBucket(ByteArrayOutputStream bucket) {
+        bucket.reset();
+        recycledBucketPool.offer(bucket);
+    }
+
+    /**
+     * Return the filled buckets
+     */
+    public BlockingDeque<ByteArrayOutputStream> getFilledBuckets() {
+        return filledBuckets;
+    }
+
+    /**
+     * Number of discarded buckets. Monitoring oriented metric.
+     */
+    public int getDiscardedBucketCount() {
+        return discardedBucketCount.get();
+    }
+
+    public long getCurrentOutputStreamSize() {
+        long sizeInBytes = 0;
+        for (ByteArrayOutputStream bucket : filledBuckets) {
+            sizeInBytes += bucket.size();
+        }
+        sizeInBytes += currentBucket.size();
+        return sizeInBytes;
+    }
+
+    @Override
+    public String toString() {
+        return "DiscardingRollingOutputStream{" +
+                "currentBucket.bytesWritten=" + currentBucket.size() +
+                ", filledBuckets.size=" + filledBuckets.size() +
+                ", discardedBucketCount=" + discardedBucketCount +
+                ", recycledBucketPool.size=" + recycledBucketPool.size() +
+                '}';
+    }
+}
