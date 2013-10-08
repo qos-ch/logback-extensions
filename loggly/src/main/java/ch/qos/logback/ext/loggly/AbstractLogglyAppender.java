72da782c79cbd555a5fb48bad6f57bad595323f9
Dan Abdinoor <abdinoor@gmail.com>
2013-10-08 14:35:28 -0400

    Update the hostname for loggly.

--- loggly/src/main/java/ch/qos/logback/ext/loggly/AbstractLogglyAppender.java
+++ loggly/src/main/java/ch/qos/logback/ext/loggly/AbstractLogglyAppender.java
@@ -35,7 +35,7 @@ import java.nio.charset.Charset;
  * @author <a href="mailto:cleclerc@xebia.fr">Cyrille Le Clerc</a>
  */
 public abstract class AbstractLogglyAppender<E> extends UnsynchronizedAppenderBase<E> {
-    public static final String DEFAULT_ENDPOINT_PREFIX = "https://logs.loggly.com/inputs/";
+    public static final String DEFAULT_ENDPOINT_PREFIX = "https://logs-01.loggly.com/inputs/";
     public static final String DEFAULT_LAYOUT_PATTERN = "%d{\"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'\",UTC} %-5level [%thread] %logger: %m%n";
     protected static final Charset UTF_8 = Charset.forName("UTF-8");
     protected String endpointUrl;
