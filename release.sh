2377d23f1d4d0ef9b4992a353203dbf14768b2c8
Tony Trinh <tony19@gmail.com>
2014-03-08 13:06:29 -0600

    Enlighten release script

--- release.sh
+++ release.sh
@@ -1,3 +1,4 @@
+#!/bin/sh -e
 #
 # Copyright (C) 2014 The logback-extensions developers (logback-user@qos.ch)
 #
@@ -14,5 +15,54 @@
 # limitations under the License.
 #
 
-mvn release:prepare
-mvn release:perform
+# -----------------------------------------------------------------------------
+# This script performs a logback-extensions release:
+#  * sets the version (removes SNAPSHOT suffix from current version, and bumps
+#    to the next SNAPSHOT version when release is finished)
+#  * tags the source in GitHub (e.g., with "v_1.0.2")
+#  * deploys the signed jars to Sonatype
+#  * updates the README.md with the current version info
+#
+# Usage:
+#   ./release.sh [--dryrun]
+#
+# -----------------------------------------------------------------------------
+version=$(mvn help:evaluate -Dexpression=project.version | grep '^[^[]')
+version=${version%-SNAPSHOT}
+outdir=$PWD/target
+readme=$PWD/README.md
+
+if [ "x$1" == "x--dryrun" ]; then
+  echo "[dryrun] just a test!"
+  dryrun=true
+fi
+echo "Starting release process for logback-extensions ${version}..."
+
+#
+# Build the JAR and print its SHA1. The last line uses GNU sed (gsed)
+# to update the README with the current release version.
+#
+mvnDryRun=
+if [ ! ${dryrun} ]; then
+  mvnDryRun=-DdryRun
+fi
+
+mvn -Dtag=v_${version} $mvnDryRun release:clean release:prepare release:perform
+
+# Update the version number in the README
+echo "Updating README.md..."
+gsed -i -e "s/\\d\+\.\\d\+\.\\d\+/${version}/" ${readme}
+
+if [ ! ${dryrun} ]; then
+  git add ${readme}
+  git commit -m "Update README for release ${version}"
+else
+  echo '[dryrun] skip commit README...'
+fi
+
+if [ ! ${dryrun} ]; then
+  echo Done. Push changes to GitHub!!
+else
+  echo Done...just a dryrun!!
+  mvn release:clean
+fi
