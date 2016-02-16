97c67c2c2f56f7cc0acdf947041bbbc57df9ea58
John Gibson <jgibson@mitre.org>
2016-02-15 20:22:04 -0500

    A hack to let MITRE's copyright information to pass the license checks.

--- spring/src/test/java/ch/qos/logback/ext/spring/MissingConfigurationTest.java
+++ spring/src/test/java/ch/qos/logback/ext/spring/MissingConfigurationTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2016 The MITRE Corporation
+ * Copyright (C) 2016 The MITRE Corporation (jgibson@mitre.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
